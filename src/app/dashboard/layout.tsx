50318b4810f318dbb67fbd5c4e0b712e260c5799
Bishal Deb <102280193+bishald0013@users.noreply.github.com>
2024-12-29 16:34:18 +0530

    new layout chnage

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
       <div className="dashboard-container min-h-screen">
-        <header className="p-4 text-dark container mx-auto flex justify-between items-center mt-5">
+        {/* <header className="p-4 text-dark container mx-auto flex justify-between items-center mt-5">
             <h1 className="text-2xl font-bold ml-4">
               <Link href="/dashboard">Relyx</Link>
             </h1>
@@ -18,7 +18,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
                 <h1 className="cursor-pointer hover:text-blue-500 transition">FAQs</h1>
                 <h1 className="cursor-pointer hover:text-blue-500 transition">Blogs</h1>
             </nav>
-        </header>
+        </header> */}
         <main className="p-6">{children}</main>
       </div>
     );
